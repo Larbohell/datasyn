1f1cee28313b5f05e864b163659e2086a55a2d41
Lars Borge Hellesylt <lars.hellesylt@gmail.com>
2017-03-27 11:49:24 +0200

    Initial commit, add first part of tutorial

---
+++ project.py
@@ -0,0 +1,46 @@
+import os
+import random
+import skimage.data
+import skimage.transform
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+# Allow image embeding in notebook
+%matplotlib inline
+
+
+def load_data(data_dir):
+    """Loads a data set and returns two lists:
+
+    images: a list of Numpy arrays, each representing an image.
+    labels: a list of numbers that represent the images labels.
+    """
+    # Get all subdirectories of data_dir. Each represents a label.
+    directories = [d for d in os.listdir(data_dir)
+                   if os.path.isdir(os.path.join(data_dir, d))]
+    # Loop through the label directories and collect the data in
+    # two lists, labels and images.
+    labels = []
+    images = []
+    for d in directories:
+        label_dir = os.path.join(data_dir, d)
+        file_names = [os.path.join(label_dir, f)
+                      for f in os.listdir(label_dir) if f.endswith(".ppm")]
+        # For each label, load it's images and add them to the images list.
+        # And add the label number (i.e. directory name) to the labels list.
+        for f in file_names:
+            images.append(skimage.data.imread(f))
+            labels.append(int(d))
+    return images, labels
+
+
+# Load training and testing datasets.
+ROOT_PATH = "/traffic"
+train_data_dir = os.path.join(ROOT_PATH, "datasets/BelgiumTS/Training")
+test_data_dir = os.path.join(ROOT_PATH, "datasets/BelgiumTS/Testing")
+
+images, labels = load_data(train_data_dir)
+
+print("Unique Labels: {0}\nTotal Images: {1}".format(len(set(labels)), len(images)))
