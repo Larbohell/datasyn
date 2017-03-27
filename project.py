a248db0cd60a6e6c6ea366070e9a0e22c1945ee2
Bendik Bjørndal Iversen <bendikiv@gmail.com>
2017-03-27 14:23:50 +0200

    Pasted all until step 12 in the tutorial

--- project.py
+++ project.py
@@ -11,7 +11,9 @@ from IPython import get_ipython
 
 # Allow image embeding in notebook
 #%matplotlib inline
-get_ipython().run_line_magic("matplotlib", "inline")
+
+#from IPython import get_ipython
+#get_ipython().run_line_magic('matplotlib', 'inline')
 
 def load_data(data_dir):
     """Loads a data set and returns two lists:
@@ -39,10 +41,105 @@ def load_data(data_dir):
 
 
 # Load training and testing datasets.
-ROOT_PATH = "/traffic"
-train_data_dir = os.path.join(ROOT_PATH, "datasets/BelgiumTS/Training")
-test_data_dir = os.path.join(ROOT_PATH, "datasets/BelgiumTS/Testing")
+ROOT_PATH = "datasets"
+train_data_dir = os.path.join(ROOT_PATH, "BelgiumTS/Training")
+test_data_dir = os.path.join(ROOT_PATH, "BelgiumTS/Testing")
 
 images, labels = load_data(train_data_dir)
 
 print("Unique Labels: {0}\nTotal Images: {1}".format(len(set(labels)), len(images)))
+
+def display_images_and_labels(images, labels):
+    """Display the first image of each label."""
+    unique_labels = set(labels)
+    plt.figure(figsize=(15, 15))
+    i = 1
+    for label in unique_labels:
+        # Pick the first image for each label.
+        image = images[labels.index(label)]
+        plt.subplot(8, 8, i)  # A grid of 8 rows x 8 columns
+        plt.axis('off')
+        plt.title("Label {0} ({1})".format(label, labels.count(label)))
+        i += 1
+        _ = plt.imshow(image)
+    plt.show()
+
+#display_images_and_labels(images, labels)
+
+
+def display_label_images(images, label):
+    """Display images of a specific label."""
+    limit = 24  # show a max of 24 images
+    plt.figure(figsize=(15, 5))
+    i = 1
+
+    start = labels.index(label)
+    end = start + labels.count(label)
+    for image in images[start:end][:limit]:
+        plt.subplot(3, 8, i)  # 3 rows, 8 per row
+        plt.axis('off')
+        i += 1
+        plt.imshow(image)
+    plt.show()
+
+#display_label_images(images, 27)
+
+# Resize images
+images32 = [skimage.transform.resize(image, (32, 32))
+                for image in images]
+#display_images_and_labels(images32, labels)
+
+labels_a = np.array(labels)
+images_a = np.array(images32)
+print("labels: ", labels_a.shape, "\nimages: ", images_a.shape)
+
+# Create a graph to hold the model.
+graph = tf.Graph()
+
+# Create model in the graph.
+with graph.as_default():
+    # Placeholders for inputs and labels.
+    images_ph = tf.placeholder(tf.float32, [None, 32, 32, 3])
+    labels_ph = tf.placeholder(tf.int32, [None])
+
+    # Flatten input from: [None, height, width, channels]
+    # To: [None, height * width * channels] == [None, 3072]
+    images_flat = tf.contrib.layers.flatten(images_ph)
+
+    # Fully connected layer.
+    # Generates logits of size [None, 62]
+    logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
+
+    # Convert logits to label indexes (int).
+    # Shape [None], which is a 1D vector of length == batch_size.
+    predicted_labels = tf.argmax(logits, 1)
+
+    # Define the loss function.
+    # Cross-entropy is a good choice for classification.
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels_ph))
+
+    # Create training op.
+    train = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+    # And, finally, an initialization op to execute before training.
+    # TODO: rename to tf.global_variables_initializer() on TF 0.12.
+    #init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
+
+print("images_flat: ", images_flat)
+print("logits: ", logits)
+print("loss: ", loss)
+print("predicted_labels: ", predicted_labels)
+
+# Create a session to run the graph we created.
+session = tf.Session(graph=graph)
+
+# First step is always to initialize all variables.
+# We don't care about the return value, though. It's None.
+_ = session.run([init])
+
+for i in range(201):
+    _, loss_value = session.run([train, loss],
+                                feed_dict={images_ph: images_a, labels_ph: labels_a})
+    if i % 10 == 0:
+        print("Loss: ", loss_value)
