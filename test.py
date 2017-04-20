bec022320b0385f805467921d47995a73e8733c6
Lars Borge Hellesylt <lars.hellesylt@gmail.com>
2017-04-20 14:06:00 +0200

    Remove obsolete code

--- test.py
+++ test.py
@@ -25,24 +25,15 @@ def main():
     #train_data_dir = os.path.join(ROOT_PATH, directory, "Training")
     test_data_dir = os.path.join(ROOT_PATH, directory, "Testing")
 
-    # Load session
-    #session = restore_model(MODEL_FOLDER_PATH)
-
     session = tf.Session()
-    # labels = tf.Tensor()
     meta_file = os.path.join("output", MODEL_FOLDER_PATH, "save.ckpt.meta")
     new_saver = tf.train.import_meta_graph(meta_file)
-
     checkpoint_dir = os.path.join("output", MODEL_FOLDER_PATH)
-
     new_saver.restore(session, tf.train.latest_checkpoint(checkpoint_dir))
 
     for v in tf.global_variables():
         print(v.name)
         session.run(v)
-    #tf.variables_initializer(tf.global_variables())
-
-    #session.run(tf.global_variables_initializer())
 
     # Load the test dataset.
     test_images, test_labels = load_data(test_data_dir)
@@ -50,47 +41,29 @@ def main():
     # Transform the images, just like we did with the training set.
     test_images32 = [skimage.transform.resize(image, (IMAGE_SCALE_SIZE_X, IMAGE_SCALE_SIZE_Y))
                      for image in test_images]
-    # display_images_and_labels(test_images32, test_labels)
-
-    #tf.reset_default_graph()
-    images_ph = tf.placeholder(tf.float32, [None, IMAGE_SCALE_SIZE_X, IMAGE_SCALE_SIZE_Y, 3])
 
     # Create a graph to hold the model.
     graph = session.graph
-    #graph = tf.Graph()
-
-    # Create model in the graph.
 
     with graph.as_default():
         # # Placeholders for inputs and labels.
         images_ph = tf.placeholder(tf.float32, [None, IMAGE_SCALE_SIZE_X, IMAGE_SCALE_SIZE_Y, 3])
-        #labels_ph = tf.placeholder(tf.int32, [None])
-        #
-        # # Flatten input from: [None, height, width, channels]
-        # # To: [None, height * width * channels] == [None, 3072]
+
+        # Flatten input from: [None, height, width, channels]
+        # To: [None, height * width * channels] == [None, 3072]
         images_flat = tf.contrib.layers.flatten(images_ph)
-        #
-        # # Fully connected layer.
-        # # Generates logits of size [None, 62]
+
+        # Fully connected layer.
+        # Generates logits of size [None, 62]
         logits = tf.contrib.layers.fully_connected(images_flat, 62, tf.nn.relu)
 
-        # Convert logits to label indexes (int).
-        # Shape [None], which is a 1D vector of length == batch_size.
-        #logits = tf.get_collection("logits")[0]
-        #logitss = tf.get_variable("logitssss", shape=62)
-        #print(logits)
-        #images_ph = tf.get_collection("images_ph")
-        #print(images_ph)
-
         predicted_labels = tf.argmax(logits, 1)
-        #print(predicted_labels)
 
     session.run(tf.global_variables_initializer())
 
     # Run predictions against the full test set.
     predicted = session.run([predicted_labels],
                             feed_dict={images_ph: test_images32})[0]
-
 
     # Calculate how many matches we got.
     match_count = sum([int(y == y_) for y, y_ in zip(test_labels, predicted)])
